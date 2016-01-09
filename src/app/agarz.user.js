68629860af3642efdcfbdc22952ed94c254242e7
richirm <richirm89@hotmail.com>
2016-01-09 00:22:14 -0500

    cambios

--- src/app/agarz.user.js
+++ src/app/agarz.user.js
@@ -25,7 +25,7 @@
 // @id              agarz
 // @name            agarz extras
 // @namespace       http://www.facebook.com/richirm
-// @version         0.0.13
+// @version         0.0.14
 // @author          richirm
 // @description     Agrega funcionalidades para agarz
 // @icon            https://raw.githubusercontent.com/richirm/agarz/master/src/assets/img/t%26p.png
@@ -61,7 +61,7 @@
 
 				window.$('body').append(body);
 				window.$('head').append(head);
-			}, 2000);
+			}, 1000);
 			window.clearInterval(intervalMain);
 		}
 	}, 200);
