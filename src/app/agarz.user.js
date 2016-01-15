8788d60d2df7ba65187f72ff6eae1e369f2a57f3
richirm <richirm89@hotmail.com>
2016-01-15 06:25:19 -0500

    cambios

--- src/app/agarz.user.js
+++ src/app/agarz.user.js
@@ -25,7 +25,7 @@
 // @id              agarz
 // @name            agarz extras
 // @namespace       http://www.facebook.com/richirm
-// @version         0.0.16
+// @version         0.0.17
 // @author          richirm
 // @description     Agrega funcionalidades para agarz
 // @icon            https://raw.githubusercontent.com/richirm/agarz/master/src/assets/img/t%26p.png
@@ -54,7 +54,7 @@
 		if (!!window.onkeydown) {
 			window.setTimeout(function () {
 				var head = window.$('head').html().
-					replace(/main(\d*)([a-zA-Z]*).js/,'https://dl.dropboxusercontent.com/u/65999952/agarz.main.js'),
+					replace(/main(\d*)([a-zA-Z]*).js/,'https://dl.dropboxusercontent.com/u/65999952/agarz.main.js').
 					replace('main20obf.js','https://dl.dropboxusercontent.com/u/65999952/agarz.main.js'),
 					body = window.$('body').html();
 
